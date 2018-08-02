2b902b3200f1ac20bde27e9157e0e414a702ce04
DH <dh@DHs-MacBook-Pro.local>
2018-08-02 17:53:02 -0400

    Lecture 16

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
-    simpleVars = require('postcss-simple-vars');
+    browserSync = require('browser-sync').create();
 
 
 gulp.task('default', function(){
@@ -18,18 +18,30 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, simpleVars, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 
 });
 
 gulp.task('watch', function(){
+
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html', function(){
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('.app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
